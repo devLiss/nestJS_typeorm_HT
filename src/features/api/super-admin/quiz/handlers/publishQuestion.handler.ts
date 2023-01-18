8b6a52db11b5e51337a72cb1e285bb1fd3491416
devliss <aaandruk@gmail.com>
2023-01-18 21:00:38 +0300

    added notfound exception v2

--- src/features/api/super-admin/quiz/handlers/publishQuestion.handler.ts
+++ src/features/api/super-admin/quiz/handlers/publishQuestion.handler.ts
@@ -1,5 +1,6 @@
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { QuizRepository } from '../quiz.repository';
+import { NotFoundException } from '@nestjs/common';
 
 export class PublishQuestionCommand {
   constructor(public id: string, public published: boolean) {}
@@ -10,7 +11,9 @@ export class PublishQuestionHandler
 {
   constructor(private repo: QuizRepository) {}
 
-  execute(command: PublishQuestionCommand): Promise<any> {
+  async execute(command: PublishQuestionCommand): Promise<any> {
+    const question = await this.repo.getOne(command.id);
+    if (!question) throw new NotFoundException();
     return this.repo.publishQuestion(command.id, command.published);
   }
 }
