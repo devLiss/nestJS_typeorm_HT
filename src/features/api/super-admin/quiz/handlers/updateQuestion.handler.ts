f329a629279b17ac9b418dad012c3703b1c3fe4f
devliss <aaandruk@gmail.com>
2023-01-18 20:57:42 +0300

    added notfound exception v1

--- src/features/api/super-admin/quiz/handlers/updateQuestion.handler.ts
+++ src/features/api/super-admin/quiz/handlers/updateQuestion.handler.ts
@@ -2,6 +2,7 @@ import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { Question } from '../../../../entities/entities/Question.entity';
 import { QuizRepository } from '../quiz.repository';
 import { CreateQuestionDto } from '../dto/createQuestion.dto';
+import { NotFoundException } from '@nestjs/common';
 
 export class UpdateQuestionCommand {
   constructor(public id: string, public cqDto: CreateQuestionDto) {}
@@ -13,7 +14,9 @@ export class UpdateQuestionHandler
 {
   constructor(private repo: QuizRepository) {}
 
-  execute(command: UpdateQuestionCommand): Promise<any> {
+  async execute(command: UpdateQuestionCommand): Promise<any> {
+    const question = await this.repo.getOne(command.id);
+    if (!question) throw new NotFoundException();
     return this.repo.updateQuestion(command.id, command.cqDto);
   }
 }
