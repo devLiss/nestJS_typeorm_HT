43ac0642774a4f6eb1fb7163044276f972ec4d0d
devliss <aaandruk@gmail.com>
2023-01-28 22:54:00 +0300

    fixed bonus for getMyAll

--- src/features/api/public/pair-quiz-game/handlers/getAllMyGames.handler.ts
+++ src/features/api/public/pair-quiz-game/handlers/getAllMyGames.handler.ts
@@ -15,6 +15,33 @@ export class GetAllMyGamesHandler implements IQueryHandler<GetAllMyGamesQuery> {
       query.userId,
       query.pagination,
     );
+
+    items.forEach((item) => {
+      if (item.status == 'Finished') {
+        const firstPlayer = item.firstPlayerProgress;
+        const secondPlayer = item.secondPlayerProgress;
+
+        const lastQuestionFP = new Date(
+          firstPlayer.answers[firstPlayer.answers.length - 1].addedAt,
+        ).getTime();
+        const lastQuestionSp = new Date(
+          secondPlayer.answers[secondPlayer.answers.length - 1].addedAt,
+        ).getTime();
+        if (
+          lastQuestionFP < lastQuestionSp &&
+          firstPlayer.answers.find((item) => item.answerStatus == 'Correct')
+        ) {
+          console.log('FP is WIN');
+          item.firstPlayerProgress.score++;
+        } else {
+          if (
+            secondPlayer.answers.find((item) => item.answerStatus == 'Correct')
+          ) {
+            item.secondPlayerProgress.score++;
+          }
+        }
+      }
+    });
     return {
       pagesCount: Math.ceil(+total / query.pagination.pageSize),
       page: query.pagination.pageNumber,
