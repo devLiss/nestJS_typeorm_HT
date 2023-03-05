0511911b3673acd10bcba7aca9786434fcc2e9ad
devliss <aaandruk@gmail.com>
2023-03-05 19:44:48 +0300

    ht-28 v5

--- src/features/api/public/pair-quiz-game/cron.js
+++
@@ -1,7 +0,0 @@
-import { QuizGameService } from './quiz-game.service';
-
-export default function (req, res) {
-  const service = new QuizGameService();
-  service.finishGames()
-    res.status(200).end('Finish Game Cron!');
-}
